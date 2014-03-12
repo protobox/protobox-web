8c2009b993011e1d8329e201c1547368fdd9d145
patrickheeney <patrickheeney@gmail.com>
2014-03-11 17:25:43 -0700

    Add new instructions

--- app/views/pages/builder/show.blade.php
+++ app/views/pages/builder/show.blade.php
@@ -14,7 +14,7 @@
                     <p class="lead">Congratulations, you just built the configuration file to your right.</p>
                     <p>If you do not have protobox installed yet, please follow the <a href="http://getprotobox.com/docs/installation" target="_blank">protobox installation guide</a>. Once installed, open terminal to your protobox directory and paste the command in the textbox below. Alternatively you can click on the download button and copy the file to <code>data/config/</code> in your protobox directory.</p>
                     <p>
-                    	<input type="text" name="share_box" value="ruby protobox install '{{ Url::route('builder.show', ['id' => $id]) }}'" class="form-control" readonly="readonly">
+                    	<input type="text" name="share_box" value="vagrant protobox install '{{ Url::route('builder.show', ['id' => $id]) }}'" class="form-control" readonly="readonly">
                     </p>
                     <p>
                     	<a href="{{ URL::route('builder.edit', ['id' => $id]) }}" class="btn btn-primary">Edit</a>
